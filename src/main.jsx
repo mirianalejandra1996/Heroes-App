b5401a3bf33d5f72a4d10b952bd24ca276ecd318
alejandra arevalo <mirianalejandra@gmail.com>
2023-09-30 21:56:57 -0500

    remove path

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/heroes-exp'>
+    <BrowserRouter>
       <HeroesApp />
     </BrowserRouter>
   </React.StrictMode>,
